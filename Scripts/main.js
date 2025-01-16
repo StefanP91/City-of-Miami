75ed903a23959f46637771bd6f2e9ba24b1f1fde
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-01-16 14:25:18 +0100

    adding animation and js to KEY INITIATIVES

--- Scripts/main.js
+++ Scripts/main.js
@@ -7,25 +7,51 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Read More Button at Key Initiatives
-document.getElementById('read-more-btn').addEventListener('click', function (event) {
-    event.preventDefault();
+function toggleReadMore(button) {
+  const description = button.closest('div[id^="description"]');
+  const paragraphs = description.querySelectorAll('p:not(:first-of-type)');
 
-    const paragraphs = document.querySelectorAll('.without-card p:not(:first-of-type)');
+  const isHidden = Array.from(paragraphs).some(p => !p.classList.contains('show'));
 
-    const isHidden = Array.from(paragraphs).some(p => !p.classList.contains('show'));
+  paragraphs.forEach(p => {
+      if (isHidden) {
+          p.classList.add('show');
+      } else {
+          p.classList.remove('show');
+      }
+  });
 
-    paragraphs.forEach(p => {
-        if (isHidden) {
-            p.classList.add('show'); 
-        } else {
-            p.classList.remove('show'); 
-        }
-    });
+  const btnText = button.querySelector('span');
+  if (btnText) {
+      btnText.textContent = isHidden ? 'Read Less' : 'Read More';
+  }
+}
 
-    const btnText = this.querySelector('span');
-    if (btnText) {
-        btnText.textContent = isHidden ? 'Read Less' : 'Read More';
-    }
+// Change content on Key Initiatives
+function openDescription(id) {
+  const descriptionContainer = document.getElementById('descriptions');
+  const descriptions = descriptionContainer.querySelectorAll('div[id^="description"]');
+
+  descriptions.forEach(description => {
+      if (description.id === id) {
+
+          description.classList.add('show');
+          
+      } else {
+          description.classList.remove('show');
+      }
+  });
+}
+
+// Change active class on Key Initiatives
+const cardContainer = document.getElementById('cardContainer');
+const cards = cardContainer.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    cards.forEach(c => c.classList.remove('active'));
+    card.classList.add('active');  
+  });
 });
 
 // Accordion button
