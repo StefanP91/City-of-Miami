cec1053895f6a8ce359962df45c0d020dd045528
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-01-16 17:20:15 +0100

    animation and functionality on KEY INDUSTRIES

--- Scripts/main.js
+++ Scripts/main.js
@@ -54,6 +54,70 @@ cards.forEach(card => {
   });
 });
 
+// Read More Button at Key Industries
+function toggleReadMoreKI(button) {
+  const description = button.closest('div[id^="description"]');
+  const paragraphs = description.querySelectorAll('p:not(:first-of-type)');
+  const lists = description.querySelectorAll ('ul')
+
+  const isHidden = Array.from(paragraphs).some(p => !p.classList.contains('show'));
+
+  paragraphs.forEach(p => {
+      if (isHidden) {
+          p.classList.add('show');
+      } else {
+          p.classList.remove('show');
+      }
+  });
+
+  lists.forEach(ul => {
+    if (isHidden) {
+        ul.classList.add('show');
+    } else {
+        ul.classList.remove('show');
+    }
+});
+
+  const btnText = button.querySelector('span');
+  if (btnText) {
+      btnText.textContent = isHidden ? 'Read Less' : 'Read More';
+  }
+}
+
+// Change content and background on Key Industries
+function openDescriptionKI(id, backgroundImage) {
+  const descriptionContainer = document.getElementById('descriptionsKI');
+  const descriptions = descriptionContainer.querySelectorAll('div[id^="description"]');
+  const keyIndustriesSection = document.getElementById('keyIndustries');
+
+  descriptions.forEach(description => {
+      if (description.id === id) {
+
+          description.classList.add('show');
+          
+      } else {
+          description.classList.remove('show');
+      }
+  });
+
+  if (keyIndustriesSection) {
+    keyIndustriesSection.style.backgroundImage = `url('${backgroundImage}')`;
+    keyIndustriesSection.style.backgroundRepeat = "no-repeat";
+    keyIndustriesSection.style.backgroundSize = "cover";
+    keyIndustriesSection.style.backgroundPosition = "center";
+  } 
+}
+
+// Change active class on Key Industries
+const cardContainerKI = document.getElementById('cardContainerKI');
+const cardsKI = cardContainerKI.querySelectorAll('.card');
+cardsKI.forEach(card => {
+  card.addEventListener('click', () => {
+    cardsKI.forEach(c => c.classList.remove('active'));
+    card.classList.add('active');  
+  });
+});
+
 // Accordion button
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener('click', function () {
